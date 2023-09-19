b521e4484dd6a41d603de743f5e0dcdf95673fb1
Calvin Koepke <hello@calvinkoepke.com>
2023-09-19 13:56:19 -0600

    chore: docs

--- src/classes/providers/HandleClientProvider.abstract.class.ts
+++ src/classes/providers/HandleClientProvider.abstract.class.ts
@@ -1,9 +1,46 @@
 import { isHex } from '../../utils/hex';
 
+/**
+ * Abstract class `HandleClientProvider` serving as a template for
+ * classes that provide methods to interact with different handle providers.
+ *
+ * @template T - The type of data returned by the `getAllData` method.
+ */
 export abstract class HandleClientProvider<T = {}> {
+    /**
+     * Abstract method to retrieve the Cardano address for a given handle.
+     *
+     * @abstract
+     * @param {string} handle - The handle to look up.
+     * @returns {Promise<string>} - A promise that resolves to the Cardano address.
+     */
     abstract getCardanoAddress: (handle: string) => Promise<string>;
+
+    /**
+     * Abstract method to retrieve the Bitcoin address for a given handle.
+     *
+     * @abstract
+     * @param {string} handle - The handle to look up.
+     * @returns {Promise<string|undefined>} - A promise that resolves to the Bitcoin address or undefined if not found.
+     */
     abstract getBitcoinAddress: (handle: string) => Promise<string | undefined>;
+
+    /**
+     * Abstract method to retrieve the Ethereum address for a given handle.
+     *
+     * @abstract
+     * @param {string} handle - The handle to look up.
+     * @returns {Promise<string|undefined>} - A promise that resolves to the Ethereum address or undefined if not found.
+     */
     abstract getEthereumAddress: (handle: string) => Promise<string | undefined>;
+
+    /**
+     * Abstract method to retrieve all data associated with a given handle.
+     *
+     * @abstract
+     * @param {string} handle - The handle to look up.
+     * @returns {Promise<T>} - A promise that resolves to the handle data.
+     */
     abstract getAllData: (handle: string) => Promise<T>;
 
     /**
--- src/classes/providers/KoraLabsProvider.class.ts
+++ src/classes/providers/KoraLabsProvider.class.ts
@@ -2,15 +2,38 @@ import { IHandle } from '@koralabs/handles-public-api-interfaces';
 
 import { HandleClientProvider } from './HandleClientProvider.abstract.class';
 
+/**
+ * KoraLabsProvider class that extends HandleClientProvider and
+ * provides methods to interact with the Handle.me API.
+ *
+ * @extends {HandleClientProvider}
+ */
 export class KoraLabsProvider extends HandleClientProvider<IHandle> {
+    /** Base URL for the Handle.me API. */
     public apiurl = 'https://api.handle.me';
+
+    /** Request headers for the API. */
     private headers = new Headers();
 
+    /** Name of the API key header. */
+    private API_KEY_NAME = 'HANDLE_ME_API_KEY';
+
+    /**
+     * Creates an instance of the KoraLabsProvider.
+     *
+     * @param {string} [header_key] - Optional API key for the Handle.me API.
+     */
     constructor(header_key?: string) {
         super();
-        header_key && this.headers.append('key', header_key);
+        header_key && this.headers.append(this.API_KEY_NAME, header_key);
     }
 
+    /**
+     * Retrieves the Cardano address for a given handle.
+     *
+     * @param {string} handle - The handle to lookup.
+     * @returns {Promise<string>} - A promise that resolves to the Cardano address.
+     */
     getCardanoAddress = async (handle: string): Promise<string> => {
         const { resolved_addresses } = await fetch(`${this.apiurl}/handles/${this.resolveName(handle)}`, {
             headers: this.headers
@@ -18,6 +41,12 @@ export class KoraLabsProvider extends HandleClientProvider<IHandle> {
         return resolved_addresses.ada;
     };
 
+    /**
+     * Retrieves the Bitcoin address for a given handle.
+     *
+     * @param {string} handle - The handle to lookup.
+     * @returns {Promise<string|undefined>} - A promise that resolves to the Bitcoin address, or undefined if not found.
+     */
     getBitcoinAddress = async (handle: string): Promise<string | undefined> => {
         const { resolved_addresses } = await fetch(`${this.apiurl}/handles/${this.resolveName(handle)}`, {
             headers: this.headers
@@ -25,6 +54,12 @@ export class KoraLabsProvider extends HandleClientProvider<IHandle> {
         return resolved_addresses?.btc;
     };
 
+    /**
+     * Retrieves the Ethereum address for a given handle.
+     *
+     * @param {string} handle - The handle to lookup.
+     * @returns {Promise<string|undefined>} - A promise that resolves to the Ethereum address, or undefined if not found.
+     */
     getEthereumAddress = async (handle: string): Promise<string | undefined> => {
         const { resolved_addresses } = await fetch(`${this.apiurl}/handles/${this.resolveName(handle)}`, {
             headers: this.headers
@@ -32,6 +67,12 @@ export class KoraLabsProvider extends HandleClientProvider<IHandle> {
         return resolved_addresses?.eth;
     };
 
+    /**
+     * Retrieves all data associated with a given handle.
+     *
+     * @param {string} handle - The handle to lookup.
+     * @returns {Promise<IHandle>} - A promise that resolves to the handle data.
+     */
     getAllData = (handle: string): Promise<IHandle> => {
         return fetch(`${this.apiurl}/handles/${this.resolveName(handle)}`, {
             headers: this.headers
