91052ee29924c67337d9eb81407446211b7af0b8
Calvin Koepke <hello@calvinkoepke.com>
2023-09-20 11:24:08 -0600

    fix: mutated options object

--- src/classes/HandleClient.class.ts
+++ src/classes/HandleClient.class.ts
@@ -2,12 +2,15 @@ import { HandleClientContext, HandleClientOptions } from '../types';
 import { KoraLabsProvider } from './providers/KoraLabsProvider.class';
 
 export class HandleClient<T = KoraLabsProvider> {
+    private options: HandleClientOptions<T>;
     static policyIds = {
         [HandleClientContext.MAINNET]: ['f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a'],
         [HandleClientContext.PREVIEW]: ['8d18d786e92776c824607fd8e193ec535c79dc61ea2405ddf3b09fe3']
     };
 
-    constructor(private options: HandleClientOptions<T>) {}
+    constructor(options: HandleClientOptions<T>) {
+        this.options = { ...options };
+    }
 
     public setContext(context: HandleClientContext) {
         this.options.context = context;
